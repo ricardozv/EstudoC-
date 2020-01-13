cb1cdc4bd059616c03b8ac306fc6693fad7ce082
ricardozv <44045430+ricardozv@users.noreply.github.com>
2020-01-13 20:07:37 -0300

    Programinha média de notas

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pagueveloz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ========== Programinha de notas ============ //
+            double nota1, nota2, nota3, nota4 = 0;
+            string frase = "Digite a {0} nota: ";
+
+            Console.WriteLine(frase, 1);
+            nota1 = Convert.ToDouble (Console.ReadLine() );
+
+            Console.WriteLine(frase, 2);
+            nota2 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.WriteLine(frase, 3);
+            nota3 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.WriteLine(frase, 4);
+            nota4 = Convert.ToDouble( Console.ReadLine() );
+
+            double total, media = 0;
+            total = nota1 + nota2 + nota3 + nota4;
+            media = total / 4;
+            
+            Console.WriteLine("A média foi: " + media);
+
+
+
+        }
+    }
+}
